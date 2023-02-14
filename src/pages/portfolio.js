52fbc4bc6917e37cbd9ef3fb8e15e0807f03fe7d
Daniela <danielatomoenk@gmail.com>
2023-02-14 15:37:02 -0300

    Esqueleto do about-section

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -15,7 +15,7 @@ headerHtml.innerHTML = `
       <a href="#">Projetos</a>
       <a href="#">Contato</a>
     </div>
-    <div class="nav-bar -en-us -hide">
+    <div class="nav-bar -en-us _hide">
       <a href="#">Home</a>
       <a href="#">About</a>
       <a href="#">Technologies</a>
@@ -28,7 +28,7 @@ headerHtml.innerHTML = `
       </svg>
     </button>
   </div>
-  <div class="page-settings -hide">
+  <div class="page-settings _hide">
     <div class="language-settings">
       <p class="language">
         Idioma/Language:
