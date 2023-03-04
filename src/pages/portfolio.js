9317ed830d23bcf955a1dd5415f71881694ce5a6
Daniela <danielatomoenk@gmail.com>
2023-03-04 12:51:23 -0300

    add descrição de skills

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,7 +4,7 @@ import './portfolio.scss';
 import '../public/components/p-header/p-header.js';
 import '../public/components/p-footer/p-footer.js';
 
-// Importação de header e footer
+// Importação de header e footer / Header and Footer import
 const headerHtml = document.createElement('div');
 headerHtml.innerHTML = `
   <div class="page-menu">
@@ -70,7 +70,7 @@ footerHtml.classList.add('p-footer');
 
 document.querySelector('footer').appendChild(footerHtml);
 
-// Efeito de digitação
+// Efeito de digitação / Typing effect
 function typeWrite (element) {
   const textArray = element.innerHTML.split('');
   element.innerHTML = '';
@@ -84,7 +84,7 @@ function typeWrite (element) {
 const typeEffect = document.querySelectorAll('.auto-type');
 typeEffect.forEach(typeWrite);
 
-//Efeito de retirada de blur
+//Efeito de retirada de blur / Blur removal effect
 window.addEventListener('scroll', function() {
   var sections = document.querySelectorAll('.p-section');
   for (var i = 0; i < sections.length; i++) {
@@ -96,7 +96,7 @@ window.addEventListener('scroll', function() {
   }
 });
 
-//Efeito nos icons em tecnologias
+//Efeito nos icons em habilidades / Effects on skill icons
 const iconsController = {
   hideOtherDescriptions: (hoveredDiv) => {
     document.querySelectorAll(`.${hoveredDiv.parentNode.className} > div`).forEach(div => {
@@ -110,8 +110,8 @@ const iconsController = {
   htmlDesc: (hover) => {
     const hoveredDiv = document.querySelector('.html-description');
     if (hover) {
-      document.querySelector('.html-description .-pt-br').innerHTML = 'Html é tal coisa ptbr';
-      document.querySelector('.html-description .-en-us').innerHTML = 'Html é tal coisa enus';
+      document.querySelector('.html-description .-pt-br').innerHTML = 'HTML5 é a versão mais recente da Linguagem de Marcação de Hipertexto, que serve para estruturar, interpretar e exibir o conteúdo de um documento web por meio de tags e atributos em um navegador.';
+      document.querySelector('.html-description .-en-us').innerHTML = 'HTML5 is the latest version of the Hypertext Markup Language, which serves to structure, interpret and display the content of a web document through tags and attributes in a browser.';
       iconsController.hideOtherDescriptions(hoveredDiv);
     } else {
       document.querySelector('.html-description .-pt-br').innerHTML = htmlPtDescPlaceholder;
@@ -122,8 +122,8 @@ const iconsController = {
   cssDesc: (hover) => {
     const hoveredDiv = document.querySelector('.css-description');
     if (hover) {
-      document.querySelector('.css-description .-pt-br').innerHTML = 'Css é tal coisa ptbr';
-      document.querySelector('.css-description .-en-us').innerHTML = 'Css é tal coisa enus';
+      document.querySelector('.css-description .-pt-br').innerHTML = 'CSS3 é a versão mais recente do Cascading Style Sheets, e é uma linguagem usada para ilustrar a aparência, estilo e formato de um documento escrito em linguagem de marcação.';
+      document.querySelector('.css-description .-en-us').innerHTML = 'CSS3 is the latest version of Cascading Style Sheets, and is a language used to ilustrate the look, style and format of a documento written in markup language.';
       iconsController.hideOtherDescriptions(hoveredDiv);
     } else {
       document.querySelector('.css-description .-pt-br').innerHTML = htmlPtDescPlaceholder;
@@ -134,8 +134,8 @@ const iconsController = {
   javascriptDesc: (hover) => {
     const hoveredDiv = document.querySelector('.javascript-description');
     if (hover) {
-      document.querySelector('.javascript-description .-pt-br').innerHTML = 'javascript é tal coisa ptbr';
-      document.querySelector('.javascript-description .-en-us').innerHTML = 'javascript é tal coisa enus';
+      document.querySelector('.javascript-description .-pt-br').innerHTML = 'JavaScript é uma linguagem de programação que permite implementar recursos complexos em páginas da Web, como atualizações de conteúdo por tempo, mapas interativos, gráficos animados, entre outros.';
+      document.querySelector('.javascript-description .-en-us').innerHTML = 'JavaScript is a programming language that allows you to implement complex features on web pages like timely content updates, interactive maps, animated graphics, etc.';
       iconsController.hideOtherDescriptions(hoveredDiv);
     } else {
       document.querySelector('.javascript-description .-pt-br').innerHTML = htmlPtDescPlaceholder;
@@ -146,8 +146,8 @@ const iconsController = {
   bootstrapDesc: (hover) => {
     const hoveredDiv = document.querySelector('.bootstrap-description');
     if (hover) {
-      document.querySelector('.bootstrap-description .-pt-br').innerHTML = 'bootstrap é tal coisa ptbr';
-      document.querySelector('.bootstrap-description .-en-us').innerHTML = 'bootstrap é tal coisa enus';
+      document.querySelector('.bootstrap-description .-pt-br').innerHTML = 'Bootstrap é um framework gratuito para desenvolvimento front-end e com código-fonte aberto, projetado para facilitar o processo de desenvolvimento de sites responsivos, para dispositivos móveis, fornecendo uma coleção de sintaxes para designs de modelos.';
+      document.querySelector('.bootstrap-description .-en-us').innerHTML = 'Bootstrap is a free and open source front-end development framework, designed to ease the web development process of responsive, mobile-first websites by providing a collection of syntax for template designs.';
       iconsController.hideOtherDescriptions(hoveredDiv);
     } else {
       document.querySelector('.bootstrap-description .-pt-br').innerHTML = htmlPtDescPlaceholder;
@@ -158,8 +158,8 @@ const iconsController = {
   gitDesc: (hover) => {
     const hoveredDiv = document.querySelector('.git-description');
     if (hover) {
-      document.querySelector('.git-description .-pt-br').innerHTML = 'git é tal coisa ptbr';
-      document.querySelector('.git-description .-en-us').innerHTML = 'git é tal coisa enus';
+      document.querySelector('.git-description .-pt-br').innerHTML = 'Git é um sistema de controle de versões distribuído que rastreia alterações em qualquer conjunto de arquivos do computador, geralmente usado para coordenar o trabalho entre programadores que desenvolvem código-fonte de forma colaborativa durante o desenvolvimento de software.';
+      document.querySelector('.git-description .-en-us').innerHTML = 'Git is a distributed version control system that tracks changes in any set of computer files, usually used for coordinating work among programmers collaboratively developing source code during software development.';
       iconsController.hideOtherDescriptions(hoveredDiv);
     } else {
       document.querySelector('.git-description .-pt-br').innerHTML = htmlPtDescPlaceholder;
@@ -170,8 +170,8 @@ const iconsController = {
   viteDesc: (hover) => {
     const hoveredDiv = document.querySelector('.vite-description');
     if (hover) {
-      document.querySelector('.vite-description .-pt-br').innerHTML = 'vite é tal coisa ptbr';
-      document.querySelector('.vite-description .-en-us').innerHTML = 'vite é tal coisa enus';
+      document.querySelector('.vite-description .-pt-br').innerHTML = 'Vite é uma ferramenta de construção que visa fornecer uma experiência de desenvolvimento mais rápida e enxuta para projetos web, gerando a estrutura de código de vários frameworks e configurando bibliotecas.';
+      document.querySelector('.vite-description .-en-us').innerHTML = 'Vite is a build tool that aims to provide a faster and leaner development experience for web projects by generating code structure for many frameworks and configuring libraries.';
       iconsController.hideOtherDescriptions(hoveredDiv);
     } else {
       document.querySelector('.vite-description .-pt-br').innerHTML = htmlPtDescPlaceholder;
@@ -182,8 +182,8 @@ const iconsController = {
   sassDesc: (hover) => {
     const hoveredDiv = document.querySelector('.sass-description');
     if (hover) {
-      document.querySelector('.sass-description .-pt-br').innerHTML = 'sass é tal coisa ptbr';
-      document.querySelector('.sass-description .-en-us').innerHTML = 'sass é tal coisa enus';
+      document.querySelector('.sass-description .-pt-br').innerHTML = 'SASS (Syntactically awesome style sheets) é um pré-processador de CSS que adiciona recursos especiais como variáveis, regras aninhadas e mixins no CSS regular. Tem como objetivo tornar o processo de codificação mais simples e eficiente.';
+      document.querySelector('.sass-description .-en-us').innerHTML = 'SASS is a CSS preprocessor, which adds special features such as variables, nested rules and mixins into regular CSS. The aim is to make the coding process simpler and more efficient.';
       iconsController.hideOtherDescriptions(hoveredDiv);
     } else {
       document.querySelector('.sass-description .-pt-br').innerHTML = htmlPtDescPlaceholder;
